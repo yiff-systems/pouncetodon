4f47d4a3d9f7e99cef3a2c805f2cc8e49473ef10
Essem <smswessem@gmail.com>
2023-11-10 15:16:29 -0600

    Add notification emails for reactions

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,11 +6,11 @@ class NotificationMailer < ApplicationMailer
          :routing
 
   before_action :process_params
-  with_options only: %i(mention favourite reblog quote) do
+  with_options only: %i(mention favourite reaction reblog quote) do
     before_action :set_status
     after_action :thread_by_conversation!
   end
-  before_action :set_account, only: [:follow, :favourite, :reblog, :follow_request]
+  before_action :set_account, only: [:follow, :favourite, :reaction, :reblog, :follow_request]
   after_action :set_list_headers!
 
   before_deliver :verify_functional_user
@@ -37,6 +37,14 @@ class NotificationMailer < ApplicationMailer
 
   def favourite
     mail subject: default_i18n_subject(name: @account.acct)
+  end
+
+  def reaction
+    return unless @user.functional? && @status.present?
+
+    locale_for_account(@me) do
+      mail subject: default_i18n_subject(name: @account.acct)
+    end
   end
 
   def reblog
--- app/models/user_settings.rb
+++ app/models/user_settings.rb
@@ -52,6 +52,7 @@ class UserSettings
     setting :follow, default: true
     setting :reblog, default: false
     setting :favourite, default: false
+    setting :reaction, default: false
     setting :mention, default: true
     setting :quote, default: true
     setting :follow_request, default: true
