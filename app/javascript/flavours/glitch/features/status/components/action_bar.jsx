1c7cc4d2f754bb11514dd5461bed3b19d0458627
Essem <smswessem@gmail.com>
2024-01-14 15:47:55 -0600

    Quick fixes

--- app/javascript/flavours/glitch/features/status/components/action_bar.jsx
+++ app/javascript/flavours/glitch/features/status/components/action_bar.jsx
@@ -281,7 +281,7 @@ class ActionBar extends PureComponent {
     const canReact = signedIn && status.get('reactions').filter(r => r.get('count') > 0 && r.get('me')).size < maxReactions;
     const reactButton = (
       <IconButton
-        className='plus-icon'
+        className='add-reaction-icon'
         onClick={this.handleNoOp} // EmojiPickerDropdown handles that
         title={intl.formatMessage(messages.react)}
         disabled={!canReact}
