60ab18dc3ddf669d8bd267a82f4fac217bfb0782
Essem <smswessem@gmail.com>
2024-11-07 15:33:42 -0600

    Fix invisible reactions on detailed statuses when logged out

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -148,6 +148,7 @@ class InitialStateSerializer < ActiveModel::Serializer
       landing_page: Setting.landing_page,
       trends_enabled: Setting.trends,
       version: instance_presenter.version,
+      visible_reactions: Setting.visible_reactions,
       terms_of_service_enabled: TermsOfService.current.present?,
       local_live_feed_access: Setting.local_live_feed_access,
       remote_live_feed_access: Setting.remote_live_feed_access,
