a2929bab0fc1b2e517e85b021089bc384d5f95a1
Essem <smswessem@gmail.com>
2026-03-30 12:02:23 -0500

    Fix removing reactions from blocked posts

--- app/controllers/api/v1/statuses/reactions_controller.rb
+++ app/controllers/api/v1/statuses/reactions_controller.rb
@@ -3,6 +3,7 @@
 class Api::V1::Statuses::ReactionsController < Api::V1::Statuses::BaseController
   before_action -> { doorkeeper_authorize! :write, :'write:favourites' }
   before_action :require_user!
+  skip_before_action :set_status, only: [:destroy]
 
   def create
     ReactService.new.call(current_account, @status, params[:id])
@@ -10,7 +11,15 @@ class Api::V1::Statuses::ReactionsController < Api::V1::Statuses::BaseController
   end
 
   def destroy
-    UnreactWorker.perform_async(current_account.id, @status.id, params[:id])
+    react = current_account.status_reactions.find_by(status_id: params[:status_id], name: params[:id])
+
+    if react
+      @status = react.status
+      UnreactWorker.perform_async(current_account.id, @status.id, params[:id])
+    else
+      @status = Status.find(params[:status_id])
+      authorize @status, :show?
+    end
 
     render json: @status, serializer: REST::StatusSerializer, relationships: StatusRelationshipsPresenter.new([@status], current_account.id, reactions_map: { @status.id => false })
   rescue ActiveRecord::RecordNotFound, Mastodon::NotPermittedError
